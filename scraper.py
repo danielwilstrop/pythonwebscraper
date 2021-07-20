c8997fe312444880d3b11bdd9a03c27e35966b64
Daniel Wilstrop <daniel.wilstrop@outlook.com>
2021-07-20 19:05:19 +0100

    done, very simple to acheive basic functionality

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 url = "https://www.amazon.co.uk/Apple-iPad-Air-10-9-inch-Wi-Fi-64GB/dp/B08J66Z99G/ref=sr_1_1_sspa?dchild=1&keywords" \
       "=ipad+air&qid=1626695174&sr=8-1-spons&psc=1&spLa" \
@@ -12,8 +13,37 @@ page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-title = soup.find(id='productTitle')
+title = soup.find(id='productTitle').get_text()
+price = soup.find(id="priceblock_ourprice").get_text()
+converted_price = float(price[1:7])
 
-print(title)
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    #Use own email and password here
+    server.login(#username, #password)
+
+    subject = "Ipad is cheap!"
+    body = 'Link below for iPad air, price had fallen below £500  ' \
+           ' "https://www.amazon.co.uk/Apple-iPad-Air-10-9-inch-Wi-Fi-64GB/dp/B08J66Z99G/ref=sr_1_1_sspa?dchild=1&keywords" \
+      "=ipad+air&qid=1626695174&sr=8-1-spons&psc=1&spLa" \
+      "=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyODdGR1QwS1k2NzRWJmVuY3J5cHRlZElkPUEwMDUzODA4WUM3VEZZUDFPWkFIJmVuY3J5cHRlZEFkSWQ9QTA2Nzg3NjQzTVVYSEhJTThWUTJKJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=='
+
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(
+        'daniel.wilstrop@outlook.com',
+        'daniel.wilstrop@outlook.com',
+        msg
+    )
+
+if(converted_price < 600):
+    send_mail()
+    print("Mail Sent")
+
+
 
 
